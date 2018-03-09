5a26dde0091d7d501630c4dcbab0d18999743f37
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2018-03-09 13:14:22 -0700

    'created Cruisin from template https://github.com/vapor/api-template'

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Cruisin",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", .branch("nio")),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", .branch("nio")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
+
